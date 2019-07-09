aaa0e86c980fb8968b78cce6b1c55051c09ce093
ehioghae.eseosa <ehioghae.eseosa@VGG-LT-028>
2019-07-09 09:52:58 +0100

    initial project set up

---
+++ CosmosDb/Program.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.Azure.Documents.Client;
+using Newtonsoft.Json;
+
+namespace CosmosDb
+{
+    internal class Program
+    {
+        private const string DatabaseName = "maindb";
+        private const string ContainerName = "employee";
+        private static DocumentClient _documentClient;
+        
+        private const string Endpoint = ""; // removed because i was pushing to a public repo
+        private const string PrimaryKey = ""; // removed because i was pushing to a public repo
+        
+        public static void Main(string[] args)
+        {
+            // reference to client
+            _documentClient = new DocumentClient(new Uri(Endpoint), PrimaryKey);
+            
+            // employee entities to be added
+            var azureEmployees = new List<EmployeeEntity>
+            {
+                new EmployeeEntity { FirstName = "David", LastName = "Ade" },
+                new EmployeeEntity { FirstName = "Gina", LastName = "Kemi" },
+                new EmployeeEntity { FirstName = "George", LastName = "Dredd" },
+                new EmployeeEntity { FirstName = "Jane", LastName = "Smith" },
+                new EmployeeEntity { FirstName = "Jane", LastName = "Doe" }
+            };
+
+//            CreateEmployees(azureEmployees);
+
+            var readEmployees = ReadEmployees("Jane");
+
+            Console.WriteLine("**************************************************");
+            Console.WriteLine("Names of all the staff having class jane firstname");
+            Console.WriteLine("**************************************************");
+
+            foreach (var employee in readEmployees) Console.WriteLine(employee);
+
+            Console.WriteLine("\n\nPress any key to exit");
+            Console.ReadKey();
+        }
+        
+        private static void CreateEmployees(List<EmployeeEntity> employeeEntities)
+        {
+            employeeEntities.ForEach(employeeEntity =>
+                {
+                   _documentClient.CreateDocumentAsync(
+                        UriFactory.CreateDocumentCollectionUri(DatabaseName, ContainerName), employeeEntity).GetAwaiter().GetResult();
+                });
+        }
+
+        private static IEnumerable<EmployeeEntity> ReadEmployees(string firstname)
+        {
+            var queryOptions = new FeedOptions { MaxItemCount = 1, EnableCrossPartitionQuery = true };
+     
+            var employeeQuery = _documentClient
+                .CreateDocumentQuery<EmployeeEntity>(
+                    UriFactory.CreateDocumentCollectionUri(DatabaseName, ContainerName), queryOptions)
+                .Where(x => x.FirstName == firstname);
+
+            return employeeQuery;
+        }
+        
+        public class EmployeeEntity
+        {
+            public string FirstName { get; set; }
+            public string LastName { get; set; }
+
+            public override string ToString()
+            {
+                return JsonConvert.SerializeObject(this);
+            }
+        }
+    }
+}
---
+++ StorageAccount/Program.cs
@@ -0,0 +1,80 @@
+﻿using Microsoft.WindowsAzure.Storage;
+using Microsoft.WindowsAzure.Storage.Table;
+using System;
+using System.Collections.Generic;
+
+namespace StorageAccount
+{
+    class Program
+    {
+        private const string PartitionKey = "class";
+        private static CloudStorageAccount _storageAccount;
+        private static CloudTableClient _tableClient;
+        private static CloudTable _employees;
+
+        static void Main(string[] args)
+        {
+            // reference to the storage account
+            _storageAccount = CloudStorageAccount.Parse(""); // removed because i was pushing to a public repo
+
+            // reference to client
+            _tableClient = _storageAccount.CreateCloudTableClient();
+
+            // create a table
+            _employees = _tableClient.GetTableReference("Employees");
+
+            // employee entities to be added
+            var azureEmployees = new List<EmployeeEntity>
+            {
+                new EmployeeEntity("David", "Ade"),
+                new EmployeeEntity("Gina", "Kemi"),
+                new EmployeeEntity("George", "Dredd"),
+                new EmployeeEntity("Jane", "Smith")
+            };
+
+            //AddEmployeesToAzure(azureEmployees);
+
+            var classEmployees = ReadEmployees(PartitionKey);
+
+            Console.WriteLine("**********************************************");
+            Console.WriteLine("Names of all the staff having class partition");
+            Console.WriteLine("**********************************************");
+
+            foreach (var employee in classEmployees) Console.WriteLine(employee.RowKey);
+
+            Console.WriteLine("\n\nPress any key to exit");
+            Console.ReadKey();
+        }
+
+        private static void CreateEmployees(List<EmployeeEntity> employeeEntities)
+        {
+            TableOperation tableOperation; // for performing table operations like insert, update
+
+            employeeEntities.ForEach(employee =>
+            {
+                tableOperation = TableOperation.Insert(employee); // insert operation
+                _employees.Execute(tableOperation); // actual addition into azure table
+            });
+        }
+
+        private static IEnumerable<EmployeeEntity> ReadEmployees(string paritionKey)
+        {
+            var tableQuery = new TableQuery<EmployeeEntity>().Where(TableQuery.GenerateFilterCondition("PartitionKey", QueryComparisons.Equal, paritionKey));
+
+            return _employees.ExecuteQuery(tableQuery);
+        }
+
+        public class EmployeeEntity : TableEntity
+        {
+            public EmployeeEntity(string firstname, string lastname)
+            {
+                PartitionKey = Program.PartitionKey;
+                RowKey = $"{firstname} {lastname}";
+            }
+            public EmployeeEntity()
+            {
+
+            }
+        }
+    }
+}
